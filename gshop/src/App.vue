fad48824a5a5aeeb2966d0c0e2952669cb388829
Niskaaaa <49870606+Niskaaaa@users.noreply.github.com>
2020-09-01 23:18:57 +0800

    gshop init

---
+++ gshop/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="app">
+    <img src="./assets/logo.png">
+
+    <router-view />
+    <FooterGuide />
+</template>
+
+<script>
+/* eslint-disable indent */
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default { // eslint-disable-next-line indent
+    components: { // eslint-disable-next-line indent
+        FooterGuide
+
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ gshop/src/pages/Order/Order.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="footer_guide">
+    <!--外卖-->
+    <span class="guide_item" :class="{on: '/Msite' === $route.path}" @click="goto('/Msite')">
+        <span class="item_icon">
+            <i class="iconfont iconshop"></i>
+        </span>
+        <span>点单</span>
+    </span>
+    <!--搜索-->
+    <span>
+        ……
+    </span>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
