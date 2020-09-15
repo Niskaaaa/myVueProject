3574dc053a6853323be74b517f0497cb05b20ab4
Niskaaaa <49870606+Niskaaaa@users.noreply.github.com>
2020-09-15 23:44:10 +0800

    footer

--- myshop/src/components/FooterGuide/FooterGuide.vue
+++ myshop/src/components/FooterGuide/FooterGuide.vue
@@ -1,25 +1,25 @@
 <template>
 <div>
     <footer class="footer_guide border-1px">
-        <a href="javascript:;" class="guide_item on">
-            <span class="item_icon">
+        <a href="javascript:;" class="guide_item" :class="{on:'/msite'===$route.path}" @click="goTo('/msite')">
+            <span class=" item_icon">
                 <i class="iconfont icon-waimai"></i>
             </span>
             <span>外卖</span>
         </a>
-        <a href="javascript:;" class="guide_item">
+        <a href="javascript:;" class="guide_item" :class="{on:'/search'===$route.path}" @click="goTo('/search')">
             <span class="item_icon">
                 <i class="iconfont icon-search"></i>
             </span>
             <span>搜索</span>
         </a>
-        <a href="javascript:;" class="guide_item">
+        <a href="javascript:;" class="guide_item" :class="{on:'/order'===$route.path}" @click="goTo('/order')">
             <span class="item_icon">
                 <i class="iconfont icon-dingdan"></i>
             </span>
             <span>订单</span>
         </a>
-        <a href="javascript:;" class="guide_item">
+        <a href="javascript:;" class="guide_item" :class="{on:'/profile'===$route.path}" @click="goTo('/profile')">
             <span class="item_icon">
                 <i class="iconfont icon-geren"></i>
             </span>
@@ -36,7 +36,13 @@ export default {
     },
     created() {},
     computed: {},
-    methods: {},
+    methods: {
+
+        goTo(path) {
+            this.$router.replace(path);
+
+        }
+    },
 }
 </script>
 
