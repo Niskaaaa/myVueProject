1fe55a6fa076d152b0d26d6be0f6d717c4c405ea
Niskaaaa <49870606+Niskaaaa@users.noreply.github.com>
2020-09-17 23:51:22 +0800

    login

--- myshop/src/pages/Profile/Profile.vue
+++ myshop/src/pages/Profile/Profile.vue
@@ -1,10 +1,13 @@
 <template>
 <section class="profile">
+
     <HeaderTop title="我的">
 
     </HeaderTop>
+
     <section class="profile-number">
-        <a href="javascript:" class="profile-link">
+
+        <router-link to="/login" class="profile-link">
             <div class="profile_image">
                 <i class="iconfont icon-person"></i>
             </div>
@@ -20,7 +23,8 @@
             <span class="arrow">
                 <i class="iconfont icon-jiantou1"></i>
             </span>
-        </a>
+        </router-link>
+
     </section>
     <section class="profile_info_data border-1px">
         <ul class="info_data_list">
--- myshop/src/router/index.js
+++ myshop/src/router/index.js
@@ -4,6 +4,7 @@ import MSite from '../pages/Msite/Msite.vue'
 import Search from '../pages/Search/Search.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
 
 Vue.use(Router)
 
@@ -11,23 +12,39 @@ export default new Router({
   routes: [
     {
       path: '/msite',
-      component: MSite
+      component: MSite,
+      meta:{
+        showFooter:true
+      }
     },
     {
       path: '/search',
-      component: Search
+      component: Search,
+      meta:{
+        showFooter:true
+      }
     },
     {
       path: '/profile',
-      component: Profile
+      component: Profile,
+      meta:{
+        showFooter:true
+      }
     },
     {
       path: '/order',
-      component: Order
+      component: Order,
+      meta:{
+        showFooter:true
+      }
     },
     {
       path: '/',
       redirect: '/msite'
+    },
+    {
+      path: '/login',
+      component: Login
     },
   ]
 })
