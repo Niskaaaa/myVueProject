0cdb518ef780870abcbcc58ee13e38f9e114dd18
Niskaaaa <49870606+Niskaaaa@users.noreply.github.com>
2020-10-08 00:30:05 +0800

    shopHeader done

--- myshop/src/mock/mockServer.js
+++ myshop/src/mock/mockServer.js
@@ -3,6 +3,6 @@ import data from './data.json'
 
 Mock.mock('/goods',{code:0,data:data.goods})
 Mock.mock('/ratings',{code:0,data:data.ratings})
-Mock.mock('/info',{code:0,data:data.ginfo})
+Mock.mock('/info',{code:0,data:data.info})
 
 //不需要向外暴露
