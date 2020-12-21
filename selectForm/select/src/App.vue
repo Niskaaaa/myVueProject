480160ceee0fc491c9a54d32e61a79d37dbd224a
Niskaaaa <1162314353@qq.com>
2020-12-22 00:41:34 +0800

    select form

---
+++ selectForm/select/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <select v-model="selectOption" @change="changeOption">
+      <option
+        :value="coupon.text"
+        v-bind:key="coupon.id"
+        v-for="coupon in options"
+      >
+        {{ coupon.text }}
+      </option>
+    </select>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      options: [
+        { id: 0, text: "苹果" },
+        { id: 1, text: "香蕉" },
+        { id: 2, text: "香梨" },
+        { id: 3, text: "榴莲" },
+        { id: 4, text: "西瓜" },
+      ],
+      selectOption:""
+    };
+
+
+  },
+  methods: {
+    changeOption(){
+      console.log(this.selectOption)
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ selectForm/select/src/components/select.vue
@@ -0,0 +1,24 @@
+!<template>
+  <div id="app">
+<select :options="options" @change="handleChange">
+  </div>
+</template>
+
+<script type="text/javascript">
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: "select",
+
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
